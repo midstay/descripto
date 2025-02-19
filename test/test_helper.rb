7493d8e49cf52fa47c6de22f5b2e7a1244701410
Magnus S. Remøe <magnus@mid-stay.com>
2025-02-19 13:28:43 +0700

    test: fix test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+ENV["RAILS_ENV"] ||= "test"
+require_relative "./dummy/config/environment"
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "descripto"
 
 require "minitest/autorun"
+require "rails/test_help"
