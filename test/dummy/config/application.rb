8cbf77b881beb86131e2855d3967eb06cafb0366
Magnus S. Remøe <magnus@mid-stay.com>
2025-03-13 14:23:31 +0800

    feat: Add support for Rails ~>v8.0

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -40,5 +40,6 @@ module Dummy
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
